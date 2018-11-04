52d3ce880ca8c91c94ae07be1561797759b41fa8
evuez <helloevuez@gmail.com>
2018-11-04 09:32:30 +0100

    Add asdf install step

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,36 @@ end
 set BC_ENV_ARGS "$HOME/.bc"
 
 set PATH "$HOME/.cargo/bin" $PATH
+source ~/.asdf/asdf.fish
+
+# Dracula theme
+
+set -x COMMENT 6272a4
+set -x CYAN 8be9fd
+set -x GREEN 50fa7b
+set -x ORANGE ffb86c
+set -x PINK ff79c6
+set -x PURPLE bd93f9
+set -x RED ff5555
+set -x YELLOW f1fa8c
+
+set fish_color_autosuggestion $COMMENT
+set fish_color_command $PINK
+set fish_color_comment $COMMENT
+set fish_color_cwd $CYAN
+set fish_color_cwd_root $RED
+set fish_color_end normal
+set fish_color_error $RED
+set fish_color_escape $CYAN
+set fish_color_history_current $CYAN
+set fish_color_host normal
+set fish_color_match $GREEN
+set fish_color_normal normal
+set fish_color_operator $CYAN
+set fish_color_param normal
+set fish_color_quote $YELLOW
+set fish_color_redirection $PURPLE
+set fish_color_search_match \x2d\x2dbackground\x3d$PURPLE
+set fish_color_selection \x2d\x2dbackground\x3d$PURPLE
+set fish_color_user $PURPLE
+set fish_color_valid_path \x2d\x2dunderline
